8a8f5b9a84bf677bc7de1262ef582056f2d816c2
Aleksey Potapov <a.potapov@thirtyseventy.digital>
2017-09-12 18:51:38 +0200

    vigenere functionality added

--- caesar.c
+++ caesar.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <memory.h>
+#include <ctype.h>
 #include <string.h>
+
+int prepare_key(char * key, int keylen);
+char rotate(char c, char delta);
 
 int main(int argc, char * argv[])
 {
@@ -8,31 +13,66 @@ int main(int argc, char * argv[])
         printf("Usage: ./caesar n\n");
         return 1;
     }
-    int delta = atoi(argv[1]);
 
-    printf("plaintext:  ABCDE \n");
+    char * key = argv[1];
+    int keylen = strlen(key);
+    if (!prepare_key(key, keylen))
+    {
+        printf("Please use valid key!\n");
+        return 1;
+    }
+
+    printf("plaintext: ABCDE \n");
     char * str = "ABCDE";
 
     printf("ciphertext: ");
+    int char_count = 0;
     for (int i = 0, len = strlen(str); i < len; i++)
     {
-        char c = str[i];
-        char new_c;
-        if (c >= 'A' && c <= 'Z')
+        if (isalpha(str[i]))
         {
-            new_c = 'A' + (c -'A' + delta) % 26;
-        }
-        else if (c >= 'a' && c <= 'z')
-        {
-            new_c = 'a' + (c - 'a' + delta) % 26;
+            int delta = key[char_count % keylen];
+            int encoded = rotate(str[i], delta);
+            printf("%c", encoded);
+
+            char_count++;
         }
         else
         {
-            new_c = c;
+            printf("%c", str[i]);
         }
-        printf("%c", new_c);
     }
 
     printf("\n");
     return 0;
+}
+
+int prepare_key(char * key, int keylen)
+{
+    for (int i = 0 ; i < keylen; i++) {
+        if (!isalpha(key[i]))
+        {
+            return 0;
+        }
+
+        key[i] = toupper(key[i]) - 'A';
+    }
+
+    return 1;
+}
+
+char rotate(char c, char delta)
+{
+    if (c >= 'A' && c <= 'Z')
+    {
+        return 'A' + (c -'A' + delta) % 26;
+    }
+    else if (c >= 'a' && c <= 'z')
+    {
+        return 'a' + (c - 'a' + delta) % 26;
+    }
+    else
+    {
+        return c;
+    }
 }
